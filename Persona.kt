2461dad561c8b7db08ab950d34c38d85894237a7
Miguel Ángel Askar Rodríguez <miguel@MacBook-Air-de-Miguel.local>
2025-02-27 08:23:49 -0500

    fun imprimirEdad added

--- Persona.kt
+++ Persona.kt
@@ -1,6 +1,11 @@
-class Persona(var nombre: String) { 
+class Persona(var nombre: String, var edad: Int) { 
     //Este es un comentario para mostrat Git
     fun imprimirNombre(){
         println("Nombre : $nombre")
     }
+
+    fun imprimirEdad() {
+        println("La edad es $edad")
+    }
+
 }
